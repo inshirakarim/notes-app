bf405282ade34dfdb3b45186e2ffb52eab479989
Inshira Karim <karim.inshira@gmail.com>
2024-01-28 18:16:08 +0530

    Added database schema and defined signup route

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRouter = require("./routes/user");
+const userRouter = require('./routes/user');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use("/user", userRouter)
-app.get("/", (req, res) => res.json({msg: "hello world after the class"}));
+// Routes
+app.use('/user', userRouter);
+app.get('/', (req, res) => res.json({ msg: 'Welcome to notes app!!!!' }));
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb+srv://kariminshira:kvq8BaPCVx01XXzJ@cluster0.7dlxsvw.mongodb.net/notes', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "notes" });
+ mongoose.connect('mongodb+srv://kariminshira:kvq8BaPCVx01XXzJ@cluster0.7dlxsvw.mongodb.net/notes', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "notes" });
 
-// app.listen(3000, () => console.log('Server running on port 3000'));
 const host = '127.0.0.1'; // Bind to the local loopback interface
 const port = 3000;
 app.listen(port, host, () => {
---
+++ server/routes/user.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const {z, ZodError} = require('zod');
+const bcrypt = require('bcrypt');
+const User = require('../db/index')
+
+const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/, {
+            message: 'Password must contain at least one uppercase letter, one lowercase letter, one special character, and one number',
+        }),
+});
+
+router.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const user = await User.findOne({ username });
+    if (user) {
+        return res.status(403).json({ message: 'User already exists' });
+    } 
+    try{
+        const userData = signupSchema.parse(req.body);
+        const hashedPassword = await bcrypt.hash(userData.password, 10)
+        const newUser = new User({
+            email: userData.email,
+            password: hashedPassword
+        });
+        await newUser.save();
+        res.status(200).json({
+            success: true , msg : "Signup Successful : New User Created", data : userData  
+        });
+    }
+    catch(error){
+        if (error instanceof ZodError){
+            res.status(400).json({success: false , msg : 'validation failed'});
+        } else{
+            console.error(error);
+            res.status(500).json({success: false , msg : 'Internal Server Error'});
+        }
+    }
+    
+
+})
+
+
+
+
+module.exports = router;
